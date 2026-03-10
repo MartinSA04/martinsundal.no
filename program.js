1cab973f2351fd301915aab53c1a5e320f4ada21
Martin Sundal Aspås <martin.s.aspas@gmail.com>
2026-03-10 16:41:12 +0100

    update page

--- program.js
+++ program.js
@@ -71,7 +71,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpl_0jla5d.js
+// include: /tmp/tmphsbrkg8f.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -216,21 +216,21 @@ Module['FS_createPath']("/assets/tilesets", "route_1", true, true);
 
   })();
 
-// end include: /tmp/tmpl_0jla5d.js
-// include: /tmp/tmp747vpe8z.js
+// end include: /tmp/tmphsbrkg8f.js
+// include: /tmp/tmpyjs_bit8.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmp747vpe8z.js
-// include: /tmp/tmpz7j3zx_l.js
+  // end include: /tmp/tmpyjs_bit8.js
+// include: /tmp/tmpupe8t1sx.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmpz7j3zx_l.js
+  // end include: /tmp/tmpupe8t1sx.js
 
 
 var arguments_ = [];
